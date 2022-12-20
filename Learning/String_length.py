4d0d2fdbf63d2f72dd2968db7807104934661290
adrija24 <24adrijaroy@gmail.com>
2022-12-20 23:46:05 +0530

    Count string length in python

---
+++ Learning/String_length.py
@@ -0,0 +1,10 @@
+str = input("Enter your name:\n")
+print("The length of your name is " , len(str))
+
+#Another way
+print("The length of your name is " , len(input("Enter your name\n")))
+
+#Another way
+name = input("What is your name?\n")
+length = len(name)
+print(length)
