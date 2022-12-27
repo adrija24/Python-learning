8a5e844a2cdb13e04eeab6537d448539b87d85a5
adrija24 <24adrijaroy@gmail.com>
2022-12-27 22:27:07 +0530

    elif condition added in Python

--- Learning/IfElse.py
+++ Learning/IfElse.py
@@ -3,5 +3,12 @@ height = int(input("What is your height in cm?\n"))
 
 if height >= 120:
     print("You can ride the rollercoaster!")
+    age = int(input("Enter your age:\n"))
+    if age < 12:
+        print("Please pay 5Rs.")
+    elif age <= 18:
+        print("Please pay 10Rs.")
+    else:
+        print("Please pay 20Rs.")
 else:
     print("Sorry! You can't ride the tollercoaster.")
