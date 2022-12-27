23d574009e0c5f928780dfbd6a95aa64f4351373
adrija24 <24adrijaroy@gmail.com>
2022-12-27 17:18:06 +0530

    Number manipulation and f-String in Python

---
+++ Learning/NumberManipulation&f-String.py
@@ -0,0 +1,16 @@
+#to round the number into a whole number
+print(round(8 / 3))
+
+#to round a number to a given precision in decimal digits
+print(round(8 / 3, 2))
+print(round(3.66666666, 2))
+
+#to convert into integer without using 'int'
+print(8 // 3)
+print(type(8 // 3))
+
+#f-String
+s = 0
+h = 4.9
+isWin = True
+print(f"Your score is {s}, your height is {h}, Your are wining is {isWin}")
