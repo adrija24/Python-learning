76ee87302c8e9eff6d604234a10129b493ce9f0e
adrija24 <24adrijaroy@gmail.com>
2022-12-27 23:18:59 +0530

    BMI calculator updated in Python

--- Learning/BmiCalculator.py
+++ Learning/BmiCalculator.py
@@ -3,4 +3,13 @@ w = input("Enter your weight in kg:\n")
 nw = float(w)
 nh = float(h)
 BMI = nw / (nh ** 2)
-print(int(BMI))
+if BMI < 18.5:
+    print(f"Your BMI is {round(BMI, 2)}, You have an underweight")
+elif BMI < 25:
+    print(f"Your BMI is {round(BMI, 2)}, you have a normal weight")
+elif BMI < 30:
+    print(f"Your BMI is {round(BMI, 2)}, you have an overweight")
+elif BMI < 35:
+    print(f"Your BMI is {round(BMI, 2)}, you have an obese")
+else:
+    print(f"Your BMI is {round(BMI, 2)}, you are clinically obese")   
