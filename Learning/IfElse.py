d7c8db1c32d711eff1a5a10d12c78d3046f10eec
adrija24 <24adrijaroy@gmail.com>
2022-12-28 09:20:52 +0530

    If else condition updated in Python

--- Learning/IfElse.py
+++ Learning/IfElse.py
@@ -1,14 +1,22 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm?\n"))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("Enter your age:\n"))
     if age < 12:
-        print("Please pay 5Rs.")
+        bill = 5
+        print(f"Please pay {bill}Rs.")
     elif age <= 18:
-        print("Please pay 10Rs.")
+        bill = 10
+        print(f"Please pay {bill}Rs.")
     else:
-        print("Please pay 20Rs.")
+        bill = 20
+        print(f"Please pay {bill}Rs.")
+    photo = input("Do you want a photo taken? Y or N.\n")
+    if photo == "Y":
+        bill += 3
+        print(f"Your final bill is {bill}Rs")
 else:
     print("Sorry! You can't ride the tollercoaster.")
