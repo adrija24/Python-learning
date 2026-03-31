67864059619b722126c58e13be5819a7a1bd4233
adrija24 <24adrijaroy@gmail.com>
2026-03-31 22:36:39 +0530

    difference between is and == in python

---
+++ Learning/is_and_==.py
@@ -0,0 +1,7 @@
+a = [1, 2, 43]
+b = [1, 2, 43]
+#python creates a new object in memory for same lists also, so they are different objects
+
+print(a is b)  # exact location of object in memory
+print(a == b)  # value of object
+
