9a4bfaa3702531160973e0d8f143b0eb5e0904e3
adrija24 <24adrijaroy@gmail.com>
2026-08-25 10:42:23 +0530

    changes done into different type of inheritance in Python

--- Learning/Inheritance.py
+++ Learning/Inheritance.py
@@ -1,72 +1,72 @@
-# class Employee:
-#     def __init__(self, name, id):
-#         self.name = name
-#         self.id = id
+class Employee:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
 
-#     def showDetails(self):
-#         print(f"The name of employee {self.id} is {self.name}")
+    def showDetails(self):
+        print(f"The name of employee {self.id} is {self.name}")
 
-# class Programmer(Employee):   #Inheritance, Programmer class is inheriting Employee class
-#     def showLanguage(self):
-#         print("the default language is Python")
-# e1 = Employee("Priya", 100)
-# e1.showDetails()
+class Programmer(Employee):   #Inheritance, Programmer class is inheriting Employee class
+    def showLanguage(self):
+        print("the default language is Python")
+e1 = Employee("Priya", 100)
+e1.showDetails()
 
-# e2 = Programmer("Ronit", 101)
-# e2.showDetails()
-# e2.showLanguage()
+e2 = Programmer("Ronit", 101)
+e2.showDetails()
+e2.showLanguage()
 
-# # Single inheritance
-# class Animal:
-#     def __init__(self, name, species):
-#         self.name = name
-#         self.species = species
-#     def make_sound(self):
-#         print("Sound made by the animal")
+# Single inheritance
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+    def make_sound(self):
+        print("Sound made by the animal")
 
-# class Dog(Animal):
-#     def __init__(self, name, breed):
-#         Animal.__init__(self, name, species="Dog")
-#         self.breed = breed
+class Dog(Animal):
+    def __init__(self, name, breed):
+        Animal.__init__(self, name, species="Dog")
+        self.breed = breed
 
-#     def make_sound(self):
-#         print("Bark!")
+    def make_sound(self):
+        print("Bark!")
 
-# d = Dog("Dog", "Doggerman")
-# d.make_sound()
+d = Dog("Dog", "Doggerman")
+d.make_sound()
 
-# a = Animal("Dog", "Dog")
-# a.make_sound()
-
-
-# # Multiple inheritance
-# class Employee:
-#     def __init__(self, name):
-#         self.name = name
-
-#     def show(self):
-#         print(f"The name of employee is {self.name}")
+a = Animal("Dog", "Dog")
+a.make_sound()
 
 
-# class Dancer:
-#     def __init__(self, dance):
-#         self.dance = dance
+# Multiple inheritance
+class Employee:
+    def __init__(self, name):
+        self.name = name
 
-#     def show(self):
-#         print(f"The dance of employee is {self.dance}")
-
-
-# class DancerEmployee(Employee, Dancer):
-#     def __init__(self, dance, name):
-#         self.dance = dance
-#         self.name = name
+    def show(self):
+        print(f"The name of employee is {self.name}")
 
 
-# obj = DancerEmployee("Bharatanatyam", "Priya")
-# print(obj.name)
-# print(obj.dance)
-# obj.show()  # This will call the show method from Employee class due to method resolution order (MRO)
-#             # i.e while inheriting we call the Employee class first
+class Dancer:
+    def __init__(self, dance):
+        self.dance = dance
+
+    def show(self):
+        print(f"The dance of employee is {self.dance}")
+
+
+class DancerEmployee(Employee, Dancer):
+    def __init__(self, dance, name):
+        self.dance = dance
+        self.name = name
+
+
+obj = DancerEmployee("Bharatanatyam", "Priya")
+print(obj.name)
+print(obj.dance)
+obj.show()  # This will call the show method from Employee class due to method resolution order (MRO)
+            # i.e while inheriting we call the Employee class first
 
 
 # Multilevel inheritance
